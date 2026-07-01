df67bb69894e46281ea966f468f1df326ee75c3f
limsuc <limsuc@gmail.com>
2026-07-01 20:46:51 +0900

    Create default-stockouts.js

---
+++ default-stockouts.js
@@ -0,0 +1,768 @@
+// Generated from the provided 서원파마 품절공지 PDF.
+window.DEFAULT_STOCKOUT_ITEMS = [
+  {
+    "maker": "대한뉴팜",
+    "productName": "록시티로신정",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "대한뉴팜",
+    "productName": "클래마신정250mg",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "대한뉴팜",
+    "productName": "라피듀오정 10/350mg",
+    "expectedDate": "6월 20일 전후"
+  },
+  {
+    "maker": "대한뉴팜",
+    "productName": "히스탄에프정 16mg/24mg",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "테라젠이텍스",
+    "productName": "다파시틴정",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "테라젠이텍스",
+    "productName": "아스피린장용정",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "테라젠이텍스",
+    "productName": "세르비아캡슐",
+    "expectedDate": "12월"
+  },
+  {
+    "maker": "테라젠이텍스",
+    "productName": "올메사탄플러스정",
+    "expectedDate": "11월"
+  },
+  {
+    "maker": "테라젠이텍스",
+    "productName": "타말겐정",
+    "expectedDate": "10월"
+  },
+  {
+    "maker": "테라젠이텍스",
+    "productName": "움카맥스시럽",
+    "expectedDate": "10월"
+  },
+  {
+    "maker": "테라젠이텍스",
+    "productName": "텔미큐어정 40mg, 80mg",
+    "expectedDate": "10월"
+  },
+  {
+    "maker": "테라젠이텍스",
+    "productName": "테라카스츄정 4mg, 5mg",
+    "expectedDate": "9월"
+  },
+  {
+    "maker": "테라젠이텍스",
+    "productName": "아메졸캡슐",
+    "expectedDate": "7월"
+  },
+  {
+    "maker": "테라젠이텍스",
+    "productName": "푸란투스정",
+    "expectedDate": "7월 초"
+  },
+  {
+    "maker": "테라젠이텍스",
+    "productName": "이텍스세픽심캡슐",
+    "expectedDate": "6월 말"
+  },
+  {
+    "maker": "테라젠이텍스",
+    "productName": "가스티렌정",
+    "expectedDate": "6월 말"
+  },
+  {
+    "maker": "국제약품",
+    "productName": "목시카신점안액(1회용) 0.4ml 12관",
+    "expectedDate": "9월"
+  },
+  {
+    "maker": "국제약품",
+    "productName": "트리티코정 50mg 500T(병)",
+    "expectedDate": "7월 초"
+  },
+  {
+    "maker": "국제약품",
+    "productName": "알레파타딘점안액 0.7% 0.3ml 12관",
+    "expectedDate": "7월 6일"
+  },
+  {
+    "maker": "국제약품",
+    "productName": "레스타포린점안액 0.05%(1회용) 0.4ml 30관",
+    "expectedDate": "7월 2일"
+  },
+  {
+    "maker": "국제약품",
+    "productName": "타겐에프연질캡슐 90C(PTP)(일반)",
+    "expectedDate": "6월 말 - 7월초"
+  },
+  {
+    "maker": "국제약품",
+    "productName": "트라디엠정 5mg 30T",
+    "expectedDate": "6월 29일"
+  },
+  {
+    "maker": "국제약품",
+    "productName": "비스메드점안액 10ml 1B",
+    "expectedDate": "6월 23-25일"
+  },
+  {
+    "maker": "국제약품",
+    "productName": "에제로바정 10/5mg 30T",
+    "expectedDate": "6월 22일"
+  },
+  {
+    "maker": "국제약품",
+    "productName": "트리티코정 25mg 100, 500T(병)",
+    "expectedDate": "6월 22일"
+  },
+  {
+    "maker": "안국약품",
+    "productName": "그래서산5mg",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "안국약품",
+    "productName": "슈클래리정500mg",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "안국약품",
+    "productName": "에소펠정20mg",
+    "expectedDate": "7월"
+  },
+  {
+    "maker": "안국약품",
+    "productName": "에이두타정 0.5mg",
+    "expectedDate": "재고 소진 후 연질캐슐 전환 예정"
+  },
+  {
+    "maker": "안국약품",
+    "productName": "에이케어정 9.96mg",
+    "expectedDate": "12월 초"
+  },
+  {
+    "maker": "안국약품",
+    "productName": "페바로정4mg",
+    "expectedDate": "7월 초"
+  },
+  {
+    "maker": "안국약품",
+    "productName": "페바로정1mg",
+    "expectedDate": "6월 말"
+  },
+  {
+    "maker": "안국약품",
+    "productName": "카복텔라점안액",
+    "expectedDate": "6월 4주차"
+  },
+  {
+    "maker": "안국약품",
+    "productName": "애니크라네오시럽",
+    "expectedDate": "6월 3주차"
+  },
+  {
+    "maker": "동구바이오",
+    "productName": "글리시타메트엑스알서방정 50/1000mg, 100/1000mg",
+    "expectedDate": "장기품절"
+  },
+  {
+    "maker": "동구바이오",
+    "productName": "동구세픽심수화물캡슐 100mg",
+    "expectedDate": "26년 12월"
+  },
+  {
+    "maker": "동구바이오",
+    "productName": "유로린정 0.1mg",
+    "expectedDate": "26년 7월 말"
+  },
+  {
+    "maker": "일성아이에스",
+    "productName": "솔페나정 5mg, 10mg",
+    "expectedDate": "입고 미정"
+  },
+  {
+    "maker": "일성아이에스",
+    "productName": "아젤블럭정 8mg",
+    "expectedDate": "입고 미정(장기품절로 정산중단, 25년 8월 EDI부터)"
+  },
+  {
+    "maker": "일화제약",
+    "productName": "오팔로정_84T",
+    "expectedDate": "6월 26일"
+  },
+  {
+    "maker": "일화제약",
+    "productName": "암로탄정 5/160mg",
+    "expectedDate": "6월 중순"
+  },
+  {
+    "maker": "셀트리온제약",
+    "productName": "아스텍션장용정 100mg 30T, 500T",
+    "expectedDate": "미정(장기품절)"
+  },
+  {
+    "maker": "셀트리온제약",
+    "productName": "레비탐정 250mg, 500mg",
+    "expectedDate": "7월 초 ~ 7월 중순"
+  },
+  {
+    "maker": "셀트리온제약",
+    "productName": "아스텍션장용정 100mg 30T, 500T",
+    "expectedDate": "미정(장기품절)"
+  },
+  {
+    "maker": "셀트리온제약",
+    "productName": "셀칼콘정",
+    "expectedDate": "7월 말 ~ 8월 초"
+  },
+  {
+    "maker": "셀트리온제약",
+    "productName": "세프리온캡슐 100mg",
+    "expectedDate": "미정(장기품절)"
+  },
+  {
+    "maker": "라이트팜텍",
+    "productName": "테넬리탁듀오서방정 10/500mg",
+    "expectedDate": "6월 중순 ~ 6월 말"
+  },
+  {
+    "maker": "삼성제약",
+    "productName": "록시나정",
+    "expectedDate": "입고일정 대기 중"
+  },
+  {
+    "maker": "삼성제약",
+    "productName": "삼성솔리페나신정 5mg, 10mg",
+    "expectedDate": "입고일정 대기 중"
+  },
+  {
+    "maker": "삼성제약",
+    "productName": "삼토젯정 10/20mg",
+    "expectedDate": "입고일정 대기 중"
+  },
+  {
+    "maker": "에이치엘비제약",
+    "productName": "클래리드정500mg",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "알리코제약",
+    "productName": "알루날서방캡슐 0.4mg",
+    "expectedDate": "확인 중"
+  },
+  {
+    "maker": "알리코제약",
+    "productName": "클로리드정 250mg",
+    "expectedDate": "확인 중"
+  },
+  {
+    "maker": "알리코제약",
+    "productName": "엘라리즈정",
+    "expectedDate": "확인 중"
+  },
+  {
+    "maker": "알리코제약",
+    "productName": "메이트정",
+    "expectedDate": "확인 중"
+  },
+  {
+    "maker": "알리코제약",
+    "productName": "크레메가연질캡슐",
+    "expectedDate": "6월 3주차"
+  },
+  {
+    "maker": "한화제약",
+    "productName": "크로마이신정500mg",
+    "expectedDate": "7월 말"
+  },
+  {
+    "maker": "마더스제약",
+    "productName": "라베엠정 10mg",
+    "expectedDate": "7월 중순~말"
+  },
+  {
+    "maker": "마더스제약",
+    "productName": "마이시딜캡슐",
+    "expectedDate": "6월 25일"
+  },
+  {
+    "maker": "마더스제약",
+    "productName": "레보설피엠정",
+    "expectedDate": "6월 19일"
+  },
+  {
+    "maker": "마더스제약",
+    "productName": "스토엠정",
+    "expectedDate": "6월 19일"
+  },
+  {
+    "maker": "바이넥스제약",
+    "productName": "모노스핀정80mg",
+    "expectedDate": "미정(장기품절) / 26년 7월 EDI까지 정산"
+  },
+  {
+    "maker": "바이넥스제약",
+    "productName": "모노스핀정40mg",
+    "expectedDate": "미정(장기품절) / 26년 6월 EDI까지 정산"
+  },
+  {
+    "maker": "진양제약",
+    "productName": "스틸란정",
+    "expectedDate": "7월 초"
+  },
+  {
+    "maker": "진양제약",
+    "productName": "아세콜연질캡슐",
+    "expectedDate": "6월 중순(재출시)"
+  },
+  {
+    "maker": "대웅바이오",
+    "productName": "대웅바이오클래리트로마이신정 250mg",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "대웅바이오",
+    "productName": "터비클린정",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "대웅바이오",
+    "productName": "리바피타정 1mg",
+    "expectedDate": "7월 중순"
+  },
+  {
+    "maker": "대웅바이오",
+    "productName": "에제탈정 10mg",
+    "expectedDate": "6월 말"
+  },
+  {
+    "maker": "대웅바이오",
+    "productName": "뮤카란캡슐 200mg",
+    "expectedDate": "6월 말"
+  },
+  {
+    "maker": "건일바이오팜",
+    "productName": "클리토신정 250mg",
+    "expectedDate": "11월 입고 예정(불순물이슈 / 신규금지)"
+  },
+  {
+    "maker": "서울제약",
+    "productName": "서울아스피린장용정",
+    "expectedDate": "미정(수탁사 생산이슈)"
+  },
+  {
+    "maker": "서울제약",
+    "productName": "알마게이트정",
+    "expectedDate": "미정(수탁사 생산이슈)"
+  },
+  {
+    "maker": "휴텍스",
+    "productName": "하이퍼벤서방정90mg",
+    "expectedDate": "7월 입고예상"
+  },
+  {
+    "maker": "휴텍스",
+    "productName": "크레스티브정 10/2.5mg",
+    "expectedDate": "6월 말"
+  },
+  {
+    "maker": "휴텍스",
+    "productName": "알레티브정 120mg",
+    "expectedDate": "6월 말"
+  },
+  {
+    "maker": "이든파마",
+    "productName": "두테스정0.5mg",
+    "expectedDate": "7월 4주차"
+  },
+  {
+    "maker": "이든파마",
+    "productName": "글레린정5mg",
+    "expectedDate": "7월 4주차"
+  },
+  {
+    "maker": "이든파마",
+    "productName": "이든알마게이트정",
+    "expectedDate": "7월 초"
+  },
+  {
+    "maker": "이든파마",
+    "productName": "이프람정5mg",
+    "expectedDate": "6월 말"
+  },
+  {
+    "maker": "이든파마",
+    "productName": "엑토린메트정15/850mg",
+    "expectedDate": "6월 중순"
+  },
+  {
+    "maker": "동국제약",
+    "productName": "솔베신정 5mg, 10mg",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "안국뉴팜",
+    "productName": "뉴트라정 / 30정 / 100정",
+    "expectedDate": "회수조치 및 공급중단"
+  },
+  {
+    "maker": "구주제약",
+    "productName": "테리아엠서방정 20/1000mg",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "구주제약",
+    "productName": "다이투스정",
+    "expectedDate": "8월 말"
+  },
+  {
+    "maker": "구주제약",
+    "productName": "돔프린정",
+    "expectedDate": "7월 말"
+  },
+  {
+    "maker": "구주제약",
+    "productName": "구주레보설피리드정",
+    "expectedDate": "7월 말"
+  },
+  {
+    "maker": "구주제약",
+    "productName": "엠포민정",
+    "expectedDate": "7월 21일"
+  },
+  {
+    "maker": "구주제약",
+    "productName": "트라마펜정",
+    "expectedDate": "7월 17일"
+  },
+  {
+    "maker": "구주제약",
+    "productName": "펜디민정",
+    "expectedDate": "7월 6일"
+  },
+  {
+    "maker": "구주제약",
+    "productName": "뮤코메드캡슐",
+    "expectedDate": "7월 1일"
+  },
+  {
+    "maker": "구주제약",
+    "productName": "디솔린정",
+    "expectedDate": "6월 24일"
+  },
+  {
+    "maker": "구주제약",
+    "productName": "스파민정",
+    "expectedDate": "6월 22일"
+  },
+  {
+    "maker": "구주제약",
+    "productName": "유나졸 캡슐",
+    "expectedDate": "6월 17일"
+  },
+  {
+    "maker": "경보제약",
+    "productName": "크라부틴듀오건조시럽",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "경보제약",
+    "productName": "크라부틴건조시럽",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "경보제약",
+    "productName": "케이마릴엠정2/500mg",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "킵스바이오파마",
+    "productName": "시프로신정",
+    "expectedDate": "미정(위탁생산업체의 원료 수급지연으로 인한 생산일정 지연)"
+  },
+  {
+    "maker": "비보존제약",
+    "productName": "비보존피나스테리드정 5mg",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "비보존제약",
+    "productName": "슈바스틴정10mg",
+    "expectedDate": "7월 말 ~ 8월 초"
+  },
+  {
+    "maker": "비보존제약",
+    "productName": "아트로젯정 10/10mg, 10/20mg",
+    "expectedDate": "7월 중순"
+  },
+  {
+    "maker": "비보존제약",
+    "productName": "비보존도네페질정10mg",
+    "expectedDate": "6월 말"
+  },
+  {
+    "maker": "비보존제약",
+    "productName": "비보존탐스로신서방정 0.2mg",
+    "expectedDate": "6월 말"
+  },
+  {
+    "maker": "비보존제약",
+    "productName": "진트로이에프정",
+    "expectedDate": "6월 중순"
+  },
+  {
+    "maker": "휴온스제약",
+    "productName": "리나디포듀오정 2.5/850mg, 2.5/500mg, 2.5/1000mg",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "팜젠사이언스",
+    "productName": "네프리스엔정 180T",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "팜젠사이언스",
+    "productName": "네프리스엔정 300T",
+    "expectedDate": "6월 25일"
+  },
+  {
+    "maker": "한국파마",
+    "productName": "노르작캡슐10mg",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "제일앤파트너스 (제일약품)",
+    "productName": "에소톤정 20mg",
+    "expectedDate": "6월 말"
+  },
+  {
+    "maker": "유니메드제약",
+    "productName": "알츠머정 5mg",
+    "expectedDate": "7월 말 입고예정"
+  },
+  {
+    "maker": "유니메드제약",
+    "productName": "파타딘큐디점안액 2.22mg 0.4ml",
+    "expectedDate": "6월 22일 입고예정"
+  },
+  {
+    "maker": "프라임제약",
+    "productName": "안플레임정",
+    "expectedDate": "7월 중 (조절공급중)"
+  },
+  {
+    "maker": "프라임제약",
+    "productName": "프라임오메가연질캡슐 60C",
+    "expectedDate": "6월 중순 (조절공급중)"
+  },
+  {
+    "maker": "프라임제약",
+    "productName": "콘드인정",
+    "expectedDate": "6월 4주차 (조절공급중)"
+  },
+  {
+    "maker": "프라임제약",
+    "productName": "라베핀정20mg",
+    "expectedDate": "6월 3주차 (조절공급중)"
+  },
+  {
+    "maker": "경동제약",
+    "productName": "유리모큐정0.1mg",
+    "expectedDate": "6월 말 입고 예정 (5.14 입고 후 전량출하)"
+  },
+  {
+    "maker": "위더스제약",
+    "productName": "레이프라정20mg",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "위더스제약",
+    "productName": "위디앙정10mg",
+    "expectedDate": "6월 말"
+  },
+  {
+    "maker": "고려제약",
+    "productName": "베타베이트액(신형)",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "제뉴원사이언스",
+    "productName": "클래이신정",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "제뉴원사이언스",
+    "productName": "듀케이겔",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "제뉴원사이언스",
+    "productName": "페북트정40mg",
+    "expectedDate": "8월 초"
+  },
+  {
+    "maker": "제뉴원사이언스",
+    "productName": "제뉴원록소프로펜나트륨정",
+    "expectedDate": "6월 22일"
+  },
+  {
+    "maker": "제뉴원사이언스",
+    "productName": "페인리스세미정",
+    "expectedDate": "6월 22일"
+  },
+  {
+    "maker": "제뉴원사이언스",
+    "productName": "시타엑스정100mg",
+    "expectedDate": "8월 초"
+  },
+  {
+    "maker": "제뉴원사이언스",
+    "productName": "트라케이듀오서방정 2.5/1000, 5/1000mg",
+    "expectedDate": "8월 초"
+  },
+  {
+    "maker": "제뉴원사이언스",
+    "productName": "엑스메트정 50/1000mg",
+    "expectedDate": "8월 중"
+  },
+  {
+    "maker": "제뉴원사이언스",
+    "productName": "빌다포스메트정 50/850mg",
+    "expectedDate": "6월 말"
+  },
+  {
+    "maker": "제뉴원사이언스",
+    "productName": "티피로신서방캡슐 0.4mg",
+    "expectedDate": "6월 4주"
+  },
+  {
+    "maker": "제뉴원사이언스",
+    "productName": "레보론정",
+    "expectedDate": "6월 22일"
+  },
+  {
+    "maker": "제뉴원사이언스",
+    "productName": "하이포지정5/80mg",
+    "expectedDate": "7월 말"
+  },
+  {
+    "maker": "제뉴원사이언스",
+    "productName": "데소나로션 0.05%",
+    "expectedDate": "7월 2일"
+  },
+  {
+    "maker": "제뉴원사이언스",
+    "productName": "프레드나로션 0.25%",
+    "expectedDate": "6월 4주"
+  },
+  {
+    "maker": "제뉴원사이언스",
+    "productName": "프로몬크림",
+    "expectedDate": "6월 19일"
+  },
+  {
+    "maker": "코오롱제약",
+    "productName": "콤비포지정 10/160mg",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "코오롱제약",
+    "productName": "알게나정",
+    "expectedDate": "6월"
+  },
+  {
+    "maker": "옵투스제약",
+    "productName": "케토핀프리점안액 0.45ml*30관",
+    "expectedDate": "8월 14일 출고예정"
+  },
+  {
+    "maker": "옵투스제약",
+    "productName": "레보플레쉬에프점안액 5ml*1병",
+    "expectedDate": "7월 7일 출고예정"
+  },
+  {
+    "maker": "옵투스제약",
+    "productName": "레보플레쉬에프점안액 0.4ml*12관",
+    "expectedDate": "6월 25일 출고예정"
+  },
+  {
+    "maker": "옵투스제약",
+    "productName": "디알프레쉬점안액 0.5ml*30관",
+    "expectedDate": "7월 7일 출고예정"
+  },
+  {
+    "maker": "옵투스제약",
+    "productName": "디쿠아프리점안액 0.45ml*60관",
+    "expectedDate": "6월 23일 출고예정"
+  },
+  {
+    "maker": "옵투스제약",
+    "productName": "디쿠아프리점안액 0.4ml*60관",
+    "expectedDate": "6월 27일 출고예정"
+  },
+  {
+    "maker": "아주약품",
+    "productName": "아나피린장용정",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "파비스제약",
+    "productName": "록사민정",
+    "expectedDate": "생산미정"
+  },
+  {
+    "maker": "파비스제약",
+    "productName": "아트라쿨정",
+    "expectedDate": "26년 6월 입고예정(세부일정미정)"
+  },
+  {
+    "maker": "대우제약",
+    "productName": "리피듀엣정 10/20mg",
+    "expectedDate": "6월 중순"
+  },
+  {
+    "maker": "지엘파마",
+    "productName": "테넬리엘정 20mg",
+    "expectedDate": "6월 말 ~7월 초 (단축 협의 중)"
+  },
+  {
+    "maker": "지엘파마",
+    "productName": "테넬리엘듀오서방정 20/1,000mg",
+    "expectedDate": "6월 중순 ~ 6월 말 (단축 협의 중)"
+  },
+  {
+    "maker": "보령제약",
+    "productName": "이지산트정 10/2.5mg",
+    "expectedDate": "6월 25일"
+  },
+  {
+    "maker": "중외신약",
+    "productName": "나무졸정",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "중외신약",
+    "productName": "멜란사민정 500mg",
+    "expectedDate": "미정"
+  },
+  {
+    "maker": "대화제약",
+    "productName": "브롤라인장용정",
+    "expectedDate": "6월 30일"
+  },
+  {
+    "maker": "명문제약",
+    "productName": "에젯토정 10/40mg",
+    "expectedDate": "6월 중순"
+  }
+];
